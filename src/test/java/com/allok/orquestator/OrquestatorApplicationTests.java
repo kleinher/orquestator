b1d062737a313e1911e1cc45816d5d9d96eda128
Hernán Kleinubing <hernan.kleinubing@despegar.com>
2024-05-03 16:10:34 +0200

    Develop (#4)
    * added postgres dialect
    
    * added h2 in memory db for testing purposes

--- src/test/java/com/allok/orquestator/OrquestatorApplicationTests.java
+++ src/test/java/com/allok/orquestator/OrquestatorApplicationTests.java
@@ -2,12 +2,14 @@ package com.allok.orquestator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class OrquestatorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+ @Test
+ void contextLoads() {
+ }
 
 }
